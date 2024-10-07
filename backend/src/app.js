dfbf4f80a16b07ae174168bfc615de77906e100e
Luiza Nobrega <mmluiza_@hotmail.com>
2024-10-07 16:19:50 +0200

    ajout du backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+// chemin à mettre pour avoir le .dotenv, copier si besoin des .env ailleurs
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../../.env') });
+// chemin fini
+
 app.use((req, res) => {
   res.json({ message: 'ça marche' });
 });
