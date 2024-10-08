5c0722dd1e7da95767568084fc6d76a5246efdba
Luiza Nobrega <mmluiza_@hotmail.com>
2024-10-08 15:32:37 +0200

    serveur et cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,13 +2,11 @@ const express = require('express');
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   dialect: 'mysql',
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
 });
-
 
 const Company = require('./models/Company')(sequelize);
 const Advertisement = require('./models/Advertisement')(sequelize);
@@ -38,7 +36,6 @@ Object.keys(models).forEach(modelName => {
   }
 });
 
-
 // Tester la connexion et synchroniser les modèles
 async function testConnection() {
   try {
@@ -52,7 +49,8 @@ async function testConnection() {
 testConnection();
 
 // Synchronisation des modèles avec la base de données
-sequelize.sync({ force: false })
+sequelize
+  .sync({ force: false })
   .then(() => {
     console.log('Les tables ont été synchronisées.');
   })
@@ -60,14 +58,28 @@ sequelize.sync({ force: false })
     console.error('Erreur lors de la synchronisation :', err);
   });
 
-
 const app = express();
 
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization',
+  );
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+// app.get('offers', (req, res, next) => {
+//   Advertisement.find()
+//     .then(advertisements => res.status(200).json(advertisements))
+//     .catch(error => res.status(400).json({ error }));
+// });
 
 app.use((req, res) => {
   res.json({ message: 'ça marche' });
 });
-
-
 
 module.exports = app;
