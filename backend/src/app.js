b057e9444e39a01140dff8fbecee63d4ad2240d0
Luiza Nobrega <mmluiza_@hotmail.com>
2024-10-08 11:21:19 +0200

    db connecte

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,25 @@
 const express = require('express');
 const app = express();
 
+const { Sequelize } = require('sequelize');
 require('dotenv').config();
+
+const sequelize = new Sequelize('', process.env.DB_USER, process.env.DB_PASS, {
+  dialect: 'mysql',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+});
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('success connexion db');
+  } catch (error) {
+    console.error('error', error);
+  }
+}
+
+testConnection();
 
 app.use((req, res) => {
   res.json({ message: 'ça marche' });
