53002ea56cfde454950f378061317cc27554ab4f
vsanti <veronique.santi@epitech.eu>
2024-10-14 16:17:59 +0200

    Account up 2/3

--- front/src/App.js
+++ front/src/App.js
@@ -38,23 +38,6 @@ function App() {
   // }, []);
 
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <h1 className="indexTitle">Job Board</h1>
-    //     {/* <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a> */}
-    //     <Advertisement />
-    //     <AdvertisementCreate />
-    //     <SignUp />
-    //     <Footer />
-    //   </header>
-    // </div>
     <>
       <Router>
         <Navigation />
